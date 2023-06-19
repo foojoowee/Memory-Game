6e90b450a5d6cc9afa8d8e7424b08cdadf950a9d
foojoowee <foo.joowee@gmail.com>
2023-06-19 22:26:43 +0800

    add: base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Memory-Game/",
+  base: "/vite-deploy/",
   plugins: [react()],
 })
